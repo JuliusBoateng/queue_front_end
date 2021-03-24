81c7336475ec4dc2b77db29df42808262740df2c
Stephen Hannon <stephenhannon4@gmail.com>
2021-03-24 01:45:55 -0400

    Delete queue

--- src/pages/SingleQueue.jsx
+++ src/pages/SingleQueue.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { API_PREFIX } from '../constants';
 
-export default function SingleQueue({ match }) {
+export default function SingleQueue({ match, history }) {
   const qid = match.params.qid;
   const [status, setStatus] = useState('Loading...');
   const [queue, setQueue] = useState();
@@ -20,16 +20,36 @@ export default function SingleQueue({ match }) {
       });
   }, [qid]);
 
+  const deleteQueue = () => {
+    if (
+      !window.confirm(
+        'Are you sure you want to delete this office hours session? This cannot be undone.'
+      )
+    ) {
+      return;
+    }
+
+    fetch(`${API_PREFIX}/queues/${qid}`, { method: 'DELETE' }).then(() => {
+      history.push('/queues');
+    });
+  };
+
   return (
     <>
-      <Link to='/queues'>Back to all office hours</Link>
+      <Link to="/queues">Back to all office hours</Link>
       <h2>Queue {qid}</h2>
       {status || (
         <>
           <p>
             {queue.course} – {queue.name} [<Link to={`${match.url}/edit`}>edit</Link>]
+            <button type="button" onClick={deleteQueue}>
+              delete
+            </button>
           </p>
-          <p>{formatRelative(new Date(queue.start), Date.now())} to {formatRelative(new Date(queue.end), Date.now())}</p>
+          <p>
+            {formatRelative(new Date(queue.start), Date.now())} to{' '}
+            {formatRelative(new Date(queue.end), Date.now())}
+          </p>
         </>
       )}
     </>
